8458cd16bff3158bcf3436762fa9ab8aa6f6a5e0
Steryff <149890281+STERYFF@users.noreply.github.com>
2024-07-21 23:38:38 +0800

    Add files via upload

---
+++ 字串索引.py
@@ -0,0 +1,15 @@
+#Python中的字串索引
+
+credit_number="1234-5678-9876-5432"
+
+first_char=credit_number[0]
+print("第一個字元:",first_char)
+
+second_char=credit_number[1]
+print("第二個字元:",second_char)
+
+first_four_char=credit_number[0:4]
+print("前四個字元:",first_four_char)
+
+last_four_char=credit_number[-4:-1]
+print("後四個字元:",last_four_char)
